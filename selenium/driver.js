116b893d4660d8de16c84319b3fcba9d5a7249a7
killias35 <killias356@gmail.com>
2026-04-27 01:25:23 +0200

    exo pret

--- selenium/driver.js
+++
@@ -1,7 +0,0 @@
-const { Builder } = require('selenium-webdriver');
-
-async function buildDriver() {
-  return await new Builder().forBrowser('chrome').build();
-}
-
-module.exports = { buildDriver };
